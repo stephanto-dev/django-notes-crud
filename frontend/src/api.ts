b4481aa2c46dc9b7b933ee031101abec0119876d
Stephanto <v.stephanto@gmail.com>
2025-05-27 09:41:02 -0300

    updated base url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "http://t00oc0c00o84g08kwg080w0c.68.183.54.121.sslip.io"
 })
 
 api.interceptors.request.use((config) => {
